842fa5dee5ffe427a90ca3b813f11c32ce6c88de
Paulo Sergio <paulosergioilha38@gmail.com>
2023-01-18 11:40:18 -0300

    Components Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header";
+import ListingBody from "./components/ListingBody";
+
 export default function App() {
-
-
   return (
-      <div></div>
+    <>
+      <Header />
+      <ListingBody />
+    </>
   );
 }
-
